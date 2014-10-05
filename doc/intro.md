38853cd367b228a54ec9294982c8ca9fb0bedff2
Igor Moiseyenko <imoiseyenko93@gmail.com>
2014-10-05 20:06:01 +0300

    MISOI project initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to misoi
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/misoi/core_test.clj
@@ -0,0 +1,7 @@
+(ns misoi.core-test
+  (:require [clojure.test :refer :all]
+            [misoi.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
